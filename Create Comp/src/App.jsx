f4c3de0fad60234e741c147c49fafc2e3a2316b5
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-12 21:08:06 +0530

    AppJSX

---
+++ Create Comp/src/App.jsx
@@ -0,0 +1,19 @@
+import Kgbutton from "./Kgbutton";
+import Hello from "./Hello";
+import Random from "./Random";
+function App(){
+return (
+  <div>
+    <h1>Hello Rudra Baby</h1>
+    <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    
+  </div>
+); 
+
+
+}
+export default App;
