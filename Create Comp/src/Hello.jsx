b18fe75a32c37423e84669cff8e4f7b5fe1f9ab9
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-12 21:08:47 +0530

    HelloFunction

---
+++ Create Comp/src/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello(){
+    let myName="Rudra"
+    let fullName = ()=>{
+    return " Rudra Arora"
+}  
+  return <h3>
+        Hello This is the future speaking {myName}{fullName()}
+    </h3>
+}
+export default Hello;
