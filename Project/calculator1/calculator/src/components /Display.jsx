d3ef15c2d64610b793441550bb916d592fe1d270
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-17 00:51:56 +0530

    Components Of Calci

---
+++ Project/calculator1/calculator/src/components /Display.jsx
@@ -0,0 +1,10 @@
+import styles  from "./Display.module.css";
+const Display =() =>{
+return (
+  <>
+    <input id="display" type="text" className={styles.display}></input>
+  </>
+);
+}
+
+export default Display;
