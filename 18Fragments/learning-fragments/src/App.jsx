bde6a68d49b22148f03f2e872c6b31a813d5b6c4
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-16 09:49:12 +0530

    Fragments

---
+++ 18Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Fooditem from "./components /FoodItems";
+import Error from "./components /Error";
+import "./App.css";
+
+function App() {
+  let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad"];
+
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <Error itemsnew={foodItems}></Error>
+      <Fooditem itemsnew={foodItems}></Fooditem>
+    </>
+  );
+}
+
+export default App;
