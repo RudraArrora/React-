3f8620c043ad2d044f7eb222ca39fe9c6eaee3eb
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-17 23:04:35 +0530

    Container Component

--- 18Fragments/learning-fragments/src/App.jsx
+++ 18Fragments/learning-fragments/src/App.jsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Fooditem from "./components /FoodItems";
 import Error from "./components /Error";
+import Container from "./components /container";
 import "./App.css";
 
 function App() {
@@ -8,9 +9,14 @@ function App() {
 
   return (
     <>
-      <h1 className="kg-span">Healthy Foods</h1>
-      <Error itemsnew={foodItems}></Error>
-      <Fooditem itemsnew={foodItems}></Fooditem>
+      <Container>
+        <h1 className="kg-span">Healthy Foods</h1>
+        <Error itemsnew={foodItems}></Error>
+        <Fooditem itemsnew={foodItems}></Fooditem>
+      </Container>
+      <Container>
+        <p>Above are the healthy food all should eat, It is important for eating good food , good food means good gut health , good glowing skin  </p>
+      </Container>
     </>
   );
 }
---
+++ 18Fragments/learning-fragments/src/components /container.jsx
@@ -0,0 +1,13 @@
+import styles from "./container.module.css"
+
+const Container = (props) => {
+  return (
+    <>
+      <div className={styles.cont}>
+       {props.children}
+      </div>
+    </>
+  );
+};
+
+export default Container;
