68d11e947f907eb87cc101cc4489b94c508b9381
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-19 16:53:32 +0530

    listitem

--- 18Fragments/learning-fragments/src/components /listitem.jsx
+++ 18Fragments/learning-fragments/src/components /listitem.jsx
@@ -1,11 +1,16 @@
-import styles from "./listitem.module.css"
-  console.log(styles);
+import styles from "./listitem.module.css";
+console.log(styles);
 function Item(props) {
   return (
     <>
-    
       <li className="list-group-item kg-item">
         <span className={styles["kg-span"]}>{props.foodItems}</span>
+        <button
+          className={`${styles.butt} btn btn-info`}
+          onClick={ props.itemHandler}
+        >
+          Buy
+        </button>
       </li>
     </>
   );
