0716561e28f7864e1a35db97a54ab9790102e2c2
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-22 21:04:51 +0530

    Todo Seeting

--- Project/TODO2/src/components /TODO.jsx
+++ Project/TODO2/src/components /TODO.jsx
@@ -1,11 +1,15 @@
-function TODO({todoname, tododate}) {
+function TODO({todoname, tododate,seeting}) {
   return (
     <div className="container ">
       <div class="row myRow">
         <div class="col-6 col-md-4">{todoname}</div>
         <div class="col-6 col-md-4">{tododate}</div>
         <div class="col-4 col-md-2">
-          <button type="button" class="btn btn-danger">
+          <button
+            type="button"
+            class="btn btn-danger"
+            onClick={() => seeting(todoname)}
+          >
             Danger
           </button>
         </div>
