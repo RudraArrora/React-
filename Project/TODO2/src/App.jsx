29ffe7ebaf46edcb1b3064d840a7a9ec80cbd4ff
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-25 21:31:08 +0530

    Reducer Dispatch

--- Project/TODO2/src/App.jsx
+++ Project/TODO2/src/App.jsx
@@ -1,8 +1,26 @@
 import AppName from "./components /AppName";
 import AppTodo from "./components /AppTodo";
 import All from "./components /All";
-import { useState } from "react";
+import { Contxt } from "./store/contextTodo";
+import { useState, useReducer } from "react";
 import "./App.css";
+
+const todoReducer = (currentTodo, action) => {
+  let newItem = currentTodo;
+  if (action.type === "New_Item") {
+    newItem = [
+      ...currentTodo,
+      {
+        name: action.payload.itemName,
+        duedate: action.payload.itemDueDate,
+      },
+    ];
+  } else if (action.type === "Delete_Item") {
+        const newItem = currentTodo.filter((item) => item.name !== action.payload.itemName);
+     
+  }
+  return newItem;
+};
 
 function App() {
   const inittodoItem = [
@@ -19,29 +37,48 @@ function App() {
       duedate: "10/02/2008",
     },
   ];
-  let [todoItem, setState] = useState(inittodoItem);
-  let onAdding = (itemName,itemDueDate,event) => {
-    console.log(itemName,itemDueDate,"are Added to the list");
-    const  newItem =  [...todoItem,{
-      name:itemName,
-      duedate:itemDueDate
-    }
-  ];
-    setState(newItem);
+  // let [todoItem, setState] = useState(inittodoItem);
+
+  const [todoItem, dispatchToDo] = useReducer(todoReducer, []);
+  let onAdding = (itemName, itemDueDate, event) => {
+    const newItemReducer = {
+      type: "New_Item",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchToDo(newItemReducer);
+    //   const  newItem =  [...todoItem,{
+    //     name:itemName,
+    //     duedate:itemDueDate
+    //   }
+    // ];
+    //   setState(newItem);
   };
 
-let seeting = (toname) => {
-  console.log("Deleted Item Is" ,toname);
-  const newFood = todoItem.filter(item =>item.name!==toname);
-  setState(newFood);
+  let seeting = (toname) => {
 
-};
+
+        const DeleteItemReducer = {
+          type: "Delete_Item",
+          payload: {
+            itemName:toname,
+          },
+        };
+        dispatchToDo(DeleteItemReducer);
+  };
+
   return (
-    <center class="todo-container">
-      <AppName />
-      <AppTodo onAdding={onAdding}></AppTodo>
-      <All todoItems={todoItem} onAdding={onAdding} seeting={seeting}></All>
-    </center>
+    <Contxt.Provider
+      value={{ todoItem: todoItem, onAdding: onAdding, seeting: seeting }}
+    >
+      <center class="todo-container">
+        <AppName />
+        <AppTodo></AppTodo>
+        <All></All>
+      </center>
+    </Contxt.Provider>
   );
 }
 
