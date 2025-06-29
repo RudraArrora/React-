3e27d199988b59fa730a39d18c682346ebc3a16b
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-29 15:22:06 +0530

    Posr Comp

--- Project/7-social-media/src/components/Post.jsx
+++ Project/7-social-media/src/components/Post.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-
-const Post = ({post}) => {
+import React, { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import {PostList} from "../store /postList";
+const Post = ({ post }) => {
+ const {deletePost} = useContext(PostList)
   return (
     <div className="card post-card" style={{ width: "18rem" }}>
       <img
@@ -9,7 +11,13 @@ const Post = ({post}) => {
         alt="Placeholder"
       />
       <div className="card-body">
-        <h5 className="card-title">{post.tittle}</h5>
+        <h5 className="card-title">
+          {post.tittle}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+          onClick={()=>deletePost(post.id)}>
+            <MdDelete />
+          </span>
+        </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
           <span class="badge text-bg-primary">{tag}</span>
