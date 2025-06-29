81d8b8f86e545e4e5c8ee46c6bbb736b736b8788
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-29 15:21:35 +0530

    Create Post Comp

--- Project/7-social-media/src/components/CreatePost.jsx
+++ Project/7-social-media/src/components/CreatePost.jsx
@@ -1,39 +1,64 @@
+import { useContext, useRef } from "react";
+import {PostList} from "../store /postList";
+
 const CreatePost = () => {
+const {addPost} = useContext(PostList);
+  const userIdE = useRef();
+  const posrtTittleE = useRef();
+  const reactionsE = useRef();
+  const bodyE = useRef();
+  const tagsE = useRef();
+
+
+const handleSubmit = (event)=>{
+event.preventDefault();
+const userId =userIdE.current.value;
+const postTittle= posrtTittleE.current.value;
+const postBody = bodyE.current.value;
+const reactions = reactionsE.current.value;
+const tags = tagsE.current.value.split(/(\s+)/);
+userIdE.current.value = " ";
+posrtTittleE.current.value = " ";
+bodyE.current.value =  " ";
+reactionsE.current.value = " ";
+tagsE.current.value =  " ";
+addPost(userId,postTittle,postBody,reactions,tags);
+  }
   return (
     <>
-      <form>
+      <form className="create-post"  onSubmit={handleSubmit}>
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
+          <label for="userId" class="form-label">
+            Enter User Id
           </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
+          <input ref={userIdE} type="text" class="form-control" id="userId" />
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
+          <label for="tittle" class="form-label">
+            Post Tittle
           </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
+          <input ref={posrtTittleE} type="text" class="form-control" id="tittle" />
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
+        <div class="mb-3">
+          <label for="body" class="form-label">
+            Post Content
           </label>
+          <textarea ref={bodyE} type="text" class="form-control" id="tittle" />
+        </div>
+        <div class="mb-3">
+          <label for="reactions" class="form-label">
+            Number Of Reactions
+          </label>
+          <textarea ref={reactionsE} type="text" class="form-control" id="reactions" />
+        </div>
+        <div class="mb-3">
+          <label for="tags" class="form-label">
+           Tags 
+          </label>
+          <textarea ref={tagsE} type="text" class="form-control" id="tags" />
         </div>
         <button type="submit" class="btn btn-primary">
-          Submit
+          Post 
         </button>
       </form>
     </>
