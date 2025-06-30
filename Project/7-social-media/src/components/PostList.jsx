564d1df46c69e338a376f1fe95615b8b8df6875a
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-30 21:56:15 +0530

    USE Effect Hook Basic Example

--- Project/7-social-media/src/components/PostList.jsx
+++ Project/7-social-media/src/components/PostList.jsx
@@ -1,20 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post"
 import WelcomeM from "./WelcomeM";
 import { PostList as PostListData } from "../store /postList";
  const PostList = () =>{
 const {postList , addPosts} = useContext(PostListData)
-const onGetPostClic = ()=>{
-fetch("https://dummyjson.com/posts")
-  .then((res) => res.json())
-  .then(data =>{
-    addPosts(data.posts)
-  });
+const [dataFetched,setDataFetched] = useState(false);
+
+if (!dataFetched){
+  fetch("https://dummyjson.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      addPosts(data.posts);
+    });
+    setDataFetched(true);
 }
+
     return (
       <>
       {
-        postList.length === 0 && <WelcomeM onGetPostClick={onGetPostClic}/>
+        postList.length === 0 && <WelcomeM />
       }
         {postList.map((post) => (
           <Post key={post.id} post={post} ></Post>
