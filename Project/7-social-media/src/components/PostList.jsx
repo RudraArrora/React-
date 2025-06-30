bfa363d0fd02a08b75046e814133482247b00d0c
Rudra Arora <arorarudra.it11@gmail.com>
2025-06-30 19:20:15 +0530

    Fetching From Dummy Api

--- Project/7-social-media/src/components/PostList.jsx
+++ Project/7-social-media/src/components/PostList.jsx
@@ -1,10 +1,21 @@
 import { useContext } from "react";
 import Post from "./Post"
+import WelcomeM from "./WelcomeM";
 import { PostList as PostListData } from "../store /postList";
  const PostList = () =>{
-const {postList} = useContext(PostListData)
+const {postList , addPosts} = useContext(PostListData)
+const onGetPostClic = ()=>{
+fetch("https://dummyjson.com/posts")
+  .then((res) => res.json())
+  .then(data =>{
+    addPosts(data.posts)
+  });
+}
     return (
       <>
+      {
+        postList.length === 0 && <WelcomeM onGetPostClick={onGetPostClic}/>
+      }
         {postList.map((post) => (
           <Post key={post.id} post={post} ></Post>
         ))}
